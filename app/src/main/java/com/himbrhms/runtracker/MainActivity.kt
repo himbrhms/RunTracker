0ec66daeeaa0d4aedb577ba834a3afa297aa6e4a
“Manuel <himbeerhummus@gmail.com>
2022-01-08 22:23:51 +0100

    adding timber logging

--- app/src/main/java/com/himbrhms/runtracker/MainActivity.kt
+++ app/src/main/java/com/himbrhms/runtracker/MainActivity.kt
@@ -9,10 +9,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.himbrhms.runtracker.ui.theme.RunTrackerTheme
+import com.himbrhms.runtracker.util.Logger
+import com.himbrhms.runtracker.util.className
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private val logger = Logger(this::class.className)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        logger.info("onCreate")
         setContent {
             RunTrackerTheme {
                 // A surface container using the 'background' color from the theme
---
+++ app/src/main/java/com/himbrhms/runtracker/util/Logger.kt
@@ -0,0 +1,30 @@
+package com.himbrhms.runtracker.util
+
+import com.himbrhms.runtracker.BuildConfig
+import timber.log.Timber
+import kotlin.reflect.KClass
+
+val <T: Any> KClass<T>.className
+    get() = if (isCompanion) this.java.declaringClass.toString() else this.qualifiedName.toString()
+
+class Logger(className: String) {
+
+    companion object {
+        init {
+            if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    private val channelName = className.removePrefix("class ").removePrefix("me.mnemllr.")
+    private val timber = Timber.tag(channelName)
+
+    fun debug(msg: String) = timber.d(msg)
+
+    fun info(msg: String) = timber.i(msg)
+
+    fun warn(msg: String) = timber.w(msg)
+
+    fun error(msg: String) = timber.e(msg)
+
+    fun fatal(msg: String, throwable: Throwable) = timber.e(throwable, msg)
+}
